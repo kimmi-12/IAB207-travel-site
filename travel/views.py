5f99f4894f242432c4b046103b79011d82e3e6f6
Kimmie <n10806199@qut.edu.au>
2024-05-11 23:08:44 +1000

    first commit

---
+++ travel/views.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from .models import Destination
+from . import db
+
+#Use of blue print to group routes, 
+# name - first argument is the blue print name 
+# import name - second argument - helps identify the root url for it 
+mainbp = Blueprint('main', __name__)
+
+@mainbp.route('/')
+def index():
+    destinations = db.session.scalars(db.select(Destination)).all() 
+    return render_template('index.html', destinations=destinations)
+
+@mainbp.route('/search')
+def search():
+    if request.args['search'] and request.args['search'] != "":
+        print(request.args['search'])
+        query = "%" + request.args['search'] + "%"
+        destinations = db.session.scalars(db.select(Destination).where(Destination.description.like(query)))
+        return render_template('index.html', destinations=destinations)
+    else:
+        return redirect(url_for('main.index'))
+
